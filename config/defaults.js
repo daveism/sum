cac3065477f3ef75441e30437bfcd7703aabedab
Dave Michelson <daveism@gmail.com>
2017-01-11 16:31:49 -0500

    new web map

--- config/defaults.js
+++ config/defaults.js
@@ -1,12 +1,12 @@
 define([], function() {
-    //Default configuration settings for the applciation. This is where you'll define things like a bing maps key, 
+    //Default configuration settings for the applciation. This is where you'll define things like a bing maps key,
     //default web map, default app color theme and more. These values can be overwritten by template configuration settings
     //and url parameters.
     var defaults = {
         "appid": "",
-        "webmap": "f172bf0e8f494d73883d74a54b1a3065", 
+        "webmap": "7e403c30b5c94335827af189596a447b", 
         "oauthappid": null, //"AFTKRmv16wj14N3z",
-        //Group templates must support a group url parameter. This will contain the id of the group. 
+        //Group templates must support a group url parameter. This will contain the id of the group.
         //group: "",
         //Enter the url to the proxy if needed by the applcation. See the 'Using the proxy page' help topic for details
         //http://developers.arcgis.com/en/javascript/jshelp/ags_proxy.html
@@ -45,7 +45,7 @@ define([], function() {
         "hideCount": false,
         "hideSummaryType": false,
         "bingmapskey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
-        "sharinghost": location.protocol + "//" + "www.arcgis.com", //Defaults to arcgis.com. Set this value to your portal or organization host name. 
+        "sharinghost": location.protocol + "//" + "www.arcgis.com", //Defaults to arcgis.com. Set this value to your portal or organization host name.
         "localize": true,
         "queryForOrg": true
     };
